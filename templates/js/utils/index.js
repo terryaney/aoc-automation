f5d4689057667be16cceb1a3aad809fb9fffdd5c
Terry Aney <terry.aney@icloud.com>
2024-12-30 14:02:34 -0600

    Updated utils library to have A* path finding

--- templates/js/utils/index.js
+++ templates/js/utils/index.js
@@ -1,79 +1,106 @@
 /**
- * Splits the raw input string into an array of lines.
+ * Parses the raw input into an array of lines.
  * @param {string} rawInput - The raw input string.
- * @returns {Array<string>} An array of lines.
+ * @returns {Array<string>} - The array of lines.
  */
 export const parseLines = (rawInput) => rawInput.split("\n");
 
 /**
- * Splits the raw input string into an array of arrays, splitting each line by the specified delimiter.
- * Optionally converts the elements to numbers.
+ * Parses the raw input into an array of arrays, splitting each line by the specified delimiter.
  * @param {string} rawInput - The raw input string.
  * @param {string} lineSplit - The delimiter to split each line.
- * @param {boolean} [toNumber=false] - Whether to convert the elements to numbers.
- * @returns {Array<Array<string | number>>} An array of arrays with the split elements.
+ * @param {boolean} [toNumber=false] - Whether to convert the split values to numbers.
+ * @returns {Array<Array<string | number>>} - The array of arrays.
  */
 export const parseLinesIntoArrays = (rawInput, lineSplit, toNumber = false) => 
-    parseLines(rawInput).map((line) => line.split(lineSplit).map((x) => toNumber ? Number(x) : x));
+    parseLines(rawInput).map((line) => 
+        line.split(lineSplit).map((x) => toNumber ? Number(x) : x)
+    );
 
 /**
- * Returns an array of movement deltas for grid navigation.
- * @param {boolean} [includeDiagonals=false] - Whether to include diagonal movements.
- * @returns {Array<MovementDelta>} An array of movement deltas.
+ * Represents movement directions and their opposites.
  */
-export const movementDeltas = (includeDiagonals) => 
-    includeDiagonals
-        ? [{ dx: -1, dy: -1, name: "NW" }, { dx: -1, dy: 0, name: "W" }, { dx: -1, dy: 1, name: "SW" }, { dx: 0, dy: -1, name: "S" }, { dx: 0, dy: 1, name: "N" }, { dx: 1, dy: -1, name: "SE" }, { dx: 1, dy: 0, name: "E" }, { dx: 1, dy: 1, name: "NE" }]
-        : [{ dx: -1, dy: 0, name: "W" }, { dx: 1, dy: 0, name: "E" }, { dx: 0, dy: -1, name: "N" }, { dx: 0, dy: 1, name: "S" }];
+export class Movement {
+    static opposites = {
+        "N": "S",
+        "S": "N",
+        "E": "W",
+        "W": "E",
+        "NE": "SW",
+        "SW": "NE",
+        "NW": "SE",
+        "SE": "NW"
+    };
+
+    /**
+     * Gets the opposite direction.
+     * @param {Direction} direction - The direction to get the opposite of.
+     * @returns {Direction} - The opposite direction.
+     */
+    static getOppositeDirection = (direction) => Movement.opposites[direction];
+
+    /**
+     * The list of movement deltas for cardinal directions.
+     * @type {Array<MovementDelta>}
+     */
+    static Directions = [
+        { dx: -1, dy: 0, direction: "W", opposite: Movement.opposites["W"] },
+        { dx: 0, dy: 1, direction: "N", opposite: Movement.opposites["N"] },
+        { dx: 1, dy: 0, direction: "E", opposite: Movement.opposites["E"] },
+        { dx: 0, dy: -1, direction: "S", opposite: Movement.opposites["S"] }
+    ];
+
+    /**
+     * The list of movement deltas for cardinal and diagonal directions.
+     * @type {Array<MovementDelta>}
+     */
+    static DirectionsWithDiagonals = [
+        { dx: -1, dy: 0, direction: "W", opposite: Movement.opposites["W"] },
+        { dx: -1, dy: -1, direction: "NW", opposite: Movement.opposites["NW"] },
+        { dx: 0, dy: 1, direction: "N", opposite: Movement.opposites["N"] },
+        { dx: 1, dy: 1, direction: "NE", opposite: Movement.opposites["NE"] },
+        { dx: 1, dy: 0, direction: "E", opposite: Movement.opposites["E"] },
+        { dx: 1, dy: -1, direction: "SE", opposite: Movement.opposites["SE"] },
+        { dx: 0, dy: -1, direction: "S", opposite: Movement.opposites["S"] },
+        { dx: -1, dy: 1, direction: "SW", opposite: Movement.opposites["SW"] },
+    ];
+}
 
 /**
- * @typedef {Object} Point
- * @property {number} x - The x-coordinate of the point.
- * @property {number} y - The y-coordinate of the point.
- * @property {TValue} value - The value of the point.
- * @property {boolean} visited - Whether the point has been visited.
- */
-
-/**
- * @typedef {Object} Grid
- * @property {Array<Array<Point<TValue>>>} points - The grid points.
- * @property {number} rows - The number of rows in the grid.
- * @property {number} cols - The number of columns in the grid.
- * @property {(point: Array<number>) => boolean} isInside - Function to check if a point is inside the grid.
- */
-
-/**
- * Parses the raw input string into a grid of points.
- * @template TValue
+ * Parses the raw input into a grid.
  * @param {string} rawInput - The raw input string.
- * @param {(value: string) => TValue} [convert] - A function to convert the cell value.
- * @param {(value: string) => boolean} [visited] - A function to determine if a cell is visited.
- * @returns {Grid<TValue>} The parsed grid.
+ * @param {function(string): TValue} [convert] - The function to convert the raw input value.
+ * @param {function(string): boolean} [visited] - The function to determine if a point is visited.
+ * @returns {Grid<TValue>} - The parsed grid.
  */
 export const parseGrid = (rawInput, convert, visited) => {
     const convertFn = convert ?? ((value) => value);
     const visitedFn = visited ?? ((value) => false);
     const points = rawInput.split("\n").map((row, y) => 
-        row.split("").map((cell, x) => ({
-            x,
-            y,
-            value: convertFn(cell),
-            visited: visitedFn(cell)
-        }))
+        row.split("").map((cell, x) => ({ x, y, value: convertFn(cell), visited: visitedFn(cell) }))
     );
     return {
         points,
         rows: points.length,
         cols: points[0].length,
-        isInside: ([x, y]) => x >= 0 && x < points[0].length && y >= 0 && y < points.length
+        isInside: ([x, y]) => x >= 0 && x < points[0].length && y >= 0 && y < points.length,
+        find: (value) => {
+            for (let y = 0; y < points.length; y++) {
+                for (let x = 0; x < points[0].length; x++) {
+                    if (points[y][x].value === value) {
+                        return points[y][x];
+                    }
+                }
+            }
+            return undefined;
+        }
     };
 };
 
 /**
- * Logs the grid to the console with an optional title.
- * @template TValue
+ * Logs the grid to the console.
  * @param {Grid<TValue>} grid - The grid to log.
- * @param {string} [title] - The optional title to display.
+ * @param {string} [title] - The title to display above the grid.
  */
 export const logGrid = (grid, title) => {
     console.log((title || "") + "\n");
@@ -82,9 +109,239 @@ export const logGrid = (grid, title) => {
     const rowDigits = String(rows - 1).length;
     const colDigits = String(cols - 1).length;
     const maxCellSpace = Math.max(colDigits, grid.points.flat().reduce((max, cell) => Math.max(max, String(cell.value).length + 1), 0));
-    console.log(`${" ".repeat(rowDigits)} | ${Array.from({ length: cols }, (_, i) => i.toString().padStart(colDigits, "0").padStart(maxCellSpace, " ")).join(" ")}`);
-    console.log(`${"-".repeat(rowDigits)}-${Array.from({ length: cols }, (_, i) => "-".padStart(maxCellSpace, "-")).join("-")}--`);
+    const logX_Axis = () => console.log(`${" ".repeat(rowDigits)} | ${Array.from({ length: cols }, (_, i) => i.toString().padStart(colDigits, "0").padStart(maxCellSpace, " ")).join(" ")}`);
+    const logHorizontalLine = () => console.log(`${"-".repeat(rowDigits)}-${Array.from({ length: cols }, (_, i) => "-".padStart(maxCellSpace, "-")).join("-")}--`);
+    logX_Axis();
+    logHorizontalLine();
     grid.points.forEach((row, y) => {
         console.log(`${y.toString().padStart(rowDigits, "0")} | ${row.map(cell => " ".repeat(maxCellSpace - 2) + (cell.visited ? "*" : " ") + cell.value).join(" ")}`);
     });
+    logHorizontalLine();
+    logX_Axis();
+    console.log("");
+};
+
+/**
+ * Performs the A* algorithm on the grid.
+ * @param {Grid<TValue>} grid - The grid to search.
+ * @param {PathNode} start - The starting node.
+ * @param {PathNode} end - The ending node.
+ * @param {Array<MovementDelta>} movementDeltas - The movement deltas.
+ * @param {function(PathNode, PathNode): number} [movementCost] - The function to calculate the movement cost.
+ * @param {function(PathNode): boolean} [canMove] - The function to determine if a move is possible.
+ * @param {boolean} [includeAllPaths] - Whether to include all paths.
+ * @returns {Array<Path> | undefined} - The found paths.
+ */
+export const aStar = (grid, start, end, movementDeltas, movementCost = (_current, _neighbor) => 1, canMove = (neighbor) => grid.points[neighbor.y][neighbor.x].value != "#", includeAllPaths = false) => {
+    start.costToFinish = manhattanDistance(start, end);
+    let isStartMovement = true;
+    const pathsFound = [];
+    const openList = [start];
+    const closedList = [];
+    const alternateParents = {};
+    while (openList.length > 0) {
+        const currentNode = openList.reduce((minNode, node) => node.totalCost < minNode.totalCost ? node : minNode, openList[0]);
+        const currentIndex = openList.indexOf(currentNode);
+        openList.splice(currentIndex, 1);
+        closedList.push(currentNode);
+        if (currentNode.x === end.x && currentNode.y === end.y) {
+            if (pathsFound.length > 0) {
+                const currentCost = currentNode.totalCost;
+                if (currentNode.totalCost > pathsFound[0].totalCost)
+                    break;
+            }
+            pathsFound.push(currentNode);
+            if (!includeAllPaths)
+                break;
+            continue;
+        }
+        const neighbors = getNeighbors(grid, currentNode, movementDeltas.filter(d => isStartMovement || d.opposite != currentNode.direction));
+        isStartMovement = false;
+        for (const neighbor of neighbors) {
+            if (!canMove(neighbor))
+                continue;
+            const closedNeighbor = closedList.find(n => n.x == neighbor.x && n.y == neighbor.y &&
+                (!includeAllPaths || n.direction == neighbor.direction || n.direction == Movement.getOppositeDirection(neighbor.direction)));
+            const costFromStart = currentNode.costFromStart + movementCost(currentNode, neighbor);
+            if (closedNeighbor) {
+                if (includeAllPaths && costFromStart == closedNeighbor.costFromStart) {
+                    addAlternateParent(closedNeighbor.clone(currentNode), alternateParents);
+                }
+                continue;
+            }
+            const openNeighbor = openList.find(n => n.x == neighbor.x && n.y == neighbor.y);
+            if (!openNeighbor || (includeAllPaths && costFromStart <= openNeighbor.costFromStart)) {
+                neighbor.costFromStart = costFromStart;
+                neighbor.costToFinish = manhattanDistance(neighbor, end);
+                neighbor.parent = currentNode;
+                openList.push(neighbor);
+            }
+            else if (costFromStart < openNeighbor.costFromStart) {
+                openNeighbor.costFromStart = costFromStart;
+                openNeighbor.parent = currentNode;
+            }
+        }
+    }
+    if (pathsFound.length == 0)
+        return undefined;
+    const allPaths = [];
+    for (const endPath of pathsFound) {
+        allPaths.push(...getPaths(endPath, alternateParents));
+    }
+    return allPaths;
+};
+
+/**
+ * Calculates the Manhattan distance between two points.
+ * @param {PathNode} a - The first point.
+ * @param {PathNode} b - The second point.
+ * @returns {number} - The Manhattan distance.
+ */
+const manhattanDistance = (a, b) => Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
+
+/**
+ * Represents a path in the grid.
+ */
+export class Path {
+    nodes;
+
+    /**
+     * Gets the length of the path.
+     * @returns {number} - The length of the path.
+     */
+    get length() { return this.nodes.length; }
+
+    /**
+     * Gets the total cost of the path.
+     * @returns {number} - The total cost of the path.
+     */
+    get totalCost() { return this.nodes[this.nodes.length - 1].totalCost; }
+
+    /**
+     * Gets the last node in the path.
+     * @returns {PathNode} - The last node in the path.
+     */
+    get lastNode() { return this.nodes[this.nodes.length - 1]; }
+
+    /**
+     * Creates a new path.
+     * @param {Array<PathNode>} nodes - The nodes in the path.
+     */
+    constructor(nodes) {
+        this.nodes = nodes;
+    }
+}
+
+/**
+ * Represents a node in a path.
+ */
+export class PathNode {
+    x;
+    y;
+    costFromStart;
+    costToFinish;
+
+    /**
+     * Gets the total cost of the node.
+     * @returns {number} - The total cost of the node.
+     */
+    get totalCost() { return this.costFromStart + this.costToFinish; }
+
+    direction;
+    parent;
+    pathIndex;
+
+    /**
+     * Creates a new path node.
+     * @param {number} x - The x coordinate of the node.
+     * @param {number} y - The y coordinate of the node.
+     * @param {Direction} direction - The direction of the node.
+     */
+    constructor(x, y, direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+        this.costFromStart = this.costToFinish = 0;
+        this.pathIndex = -1;
+    }
+
+    /**
+     * Clones the node with a new parent.
+     * @param {PathNode} parent - The parent node.
+     * @returns {PathNode} - The cloned node.
+     */
+    clone(parent) {
+        const clone = new PathNode(this.x, this.y, this.direction);
+        clone.costFromStart = this.costFromStart;
+        clone.costToFinish = this.costToFinish;
+        clone.parent = parent;
+        return clone;
+    }
+}
+
+/**
+ * Gets the neighbors of a node.
+ * @param {Grid<TValue>} grid - The grid.
+ * @param {PathNode} node - The node.
+ * @param {Array<MovementDelta>} movementDeltas - The movement deltas.
+ * @returns {Array<PathNode>} - The neighbors.
+ */
+const getNeighbors = (grid, node, movementDeltas) => {
+    const neighbors = movementDeltas.map((delta) => {
+        const x = node.x + delta.dx;
+        const y = node.y + delta.dy;
+        if (!grid.isInside([x, y]))
+            return undefined;
+        return new PathNode(x, y, delta.direction);
+    }).filter(n => n != undefined);
+    return neighbors;
+};
+
+/**
+ * Serializes a node to a string.
+ * @param {PathNode} node - The node.
+ * @returns {string} - The serialized node.
+ */
+const serializeNode = (node) => `${node?.x},${node?.y}`;
+
+/**
+ * Adds an alternate parent to a node.
+ * @param {PathNode} node - The node.
+ * @param {Object} alternateParents - The alternate parents.
+ */
+const addAlternateParent = (node, alternateParents) => {
+    const key = serializeNode(node);
+    if (!alternateParents[key]) {
+        alternateParents[key] = [node];
+        return;
+    }
+    if (alternateParents[key].some(n => n.x == node.x && n.y == node.y))
+        return;
+    alternateParents[key].push(node);
+};
+
+/**
+ * Gets the paths from a node.
+ * @param {PathNode} node - The node.
+ * @param {Object} alternateParents - The alternate parents.
+ * @returns {Array<Path>} - The paths.
+ */
+const getPaths = (node, alternateParents) => {
+    const allPaths = [];
+    const path = [];
+    let current = node;
+    while (current) {
+        path.push(current);
+        current = current.parent;
+        const alternateParent = alternateParents[serializeNode(current)];
+        if (alternateParent) {
+            const pathToEnd = [...path].reverse();
+            for (const p of alternateParent) {
+                const alternatePaths = getPaths(p, alternateParents);
+                alternatePaths.forEach(p => allPaths.push(new Path([...p.nodes, ...pathToEnd])));
+            }
+        }
+    }
+    allPaths.push(new Path(path.reverse()));
+    return allPaths;
 };
--- templates/ts/utils/index.ts
+++ templates/ts/utils/index.ts
@@ -35,18 +35,44 @@ export const parseLines = (rawInput: string): Array<string> => rawInput.split("\
 export const parseLinesIntoArrays = (rawInput: string, lineSplit: string, toNumber: boolean = false): Array<Array<string | number>> =>
 	parseLines(rawInput).map((line) => line.split(lineSplit).map((x) => toNumber ? Number(x) : x));
 
-export const movementDeltas = (includeDiagonals?: boolean) =>
-	includeDiagonals
-		? [{ dx: -1, dy: -1, name: "NW"} as MovementDelta, { dx: -1, dy: 0, name: "W"} as MovementDelta, { dx: -1, dy: 1, name: "SW"} as MovementDelta, { dx: 0, dy: -1, name: "S"} as MovementDelta, { dx: 0, dy: 1, name: "N"} as MovementDelta, { dx: 1, dy: -1, name: "SE"} as MovementDelta, { dx: 1, dy: 0, name: "E"} as MovementDelta, { dx: 1, dy: 1, name: "NE"} as MovementDelta]
-		: [{ dx: -1, dy: 0, name: "W"} as MovementDelta, { dx: 1, dy: 0, name: "E"} as MovementDelta, { dx: 0, dy: -1, name: "N"} as MovementDelta, { dx: 0, dy: 1, name: "S"} as MovementDelta];
+export class Movement {
+	private static opposites: Record<Direction, Direction> = {
+		"N": "S",
+		"S": "N",
+		"E": "W",
+		"W": "E",
+		"NE": "SW",
+		"SW": "NE",
+		"NW": "SE",
+		"SE": "NW"
+	};
+	static getOppositeDirection = (direction: Direction): Direction => Movement.opposites[direction];
+	static Directions = [
+		{ dx: -1, dy: 0, direction: "W", opposite: Movement.opposites[ "W"] } as MovementDelta,
+		{ dx: 0, dy: 1, direction: "N", opposite: Movement.opposites[ "N"] } as MovementDelta,
+		{ dx: 1, dy: 0, direction: "E", opposite: Movement.opposites[ "E"] } as MovementDelta,
+		{ dx: 0, dy: -1, direction: "S", opposite: Movement.opposites[ "S"] } as MovementDelta
+	];
+	static DirectionsWithDiagonals = [
+		{ dx: -1, dy: 0, direction: "W", opposite: Movement.opposites[ "W"] } as MovementDelta,
+		{ dx: -1, dy: -1, direction: "NW", opposite: Movement.opposites[ "NW"] } as MovementDelta,
+		{ dx: 0, dy: 1, direction: "N", opposite: Movement.opposites[ "N"] } as MovementDelta,
+		{ dx: 1, dy: 1, direction: "NE", opposite: Movement.opposites[ "NE"] } as MovementDelta,
+		{ dx: 1, dy: 0, direction: "E", opposite: Movement.opposites[ "E"] } as MovementDelta,
+		{ dx: 1, dy: -1, direction: "SE", opposite: Movement.opposites[ "SE"] } as MovementDelta,
+		{ dx: 0, dy: -1, direction: "S", opposite: Movement.opposites[ "S"] } as MovementDelta,
+		{ dx: -1, dy: 1, direction: "SW", opposite: Movement.opposites[ "SW"] } as MovementDelta,
+	];
+}
 
-export type MovementDelta = { dx: number, dy: number, name: string };
+export type MovementDelta = { dx: number, dy: number, direction: Direction, opposite: Direction };
 export type Point<TValue> = { x: number, y: number, value: TValue, visited: boolean };
 export type Grid<TValue> = {
 	points: Array<Array<Point<TValue>>>,
 	rows: number,
 	cols: number,
-	isInside: (point: Array<number>) => boolean
+	isInside: (point: Array<number>) => boolean,
+	find: (value: TValue) => Point<TValue> | undefined
 };
 
 export const parseGrid = <TValue = string>(rawInput: string, convert?: (value: string) => TValue, visited?: (value: string) => boolean): Grid<TValue> => {
@@ -58,7 +84,17 @@ export const parseGrid = <TValue = string>(rawInput: string, convert?: (value: s
 		points,
 		rows: points.length,
 		cols: points[0].length,
-		isInside: ([x, y]) => x >= 0 && x < points[0].length && y >= 0 && y < points.length
+		isInside: ([x, y]) => x >= 0 && x < points[0].length && y >= 0 && y < points.length,
+		find: (value: TValue) => {
+			for (let y = 0; y < points.length; y++) {
+				for (let x = 0; x < points[0].length; x++) {
+					if (points[y][x].value === value) {
+						return points[y][x];
+					}
+				}
+			}
+			return undefined;
+		}
 	};
 } 
 
@@ -69,10 +105,197 @@ export const logGrid = <TValue>(grid: Grid<TValue>, title?: string): void => {
 	const rowDigits = String(rows - 1).length;
 	const colDigits = String(cols - 1).length;
 	const maxCellSpace = Math.max(colDigits, grid.points.flat().reduce((max, cell) => Math.max(max, String(cell.value).length + 1), 0));
-	console.log(`${" ".repeat(rowDigits)} | ${Array.from({ length: cols }, (_, i) => i.toString().padStart(colDigits, "0").padStart(maxCellSpace, " ") ).join(" ")}`);
-	console.log(`${"-".repeat(rowDigits)}-${Array.from({ length: cols }, (_, i) => "-".padStart(maxCellSpace, "-") ).join("-")}--`);
+	const logX_Axis = () => console.log(`${" ".repeat(rowDigits)} | ${Array.from({ length: cols }, (_, i) => i.toString().padStart(colDigits, "0").padStart(maxCellSpace, " ")).join(" ")}`);
+	const logHorizontalLine = () => console.log(`${"-".repeat(rowDigits)}-${Array.from({ length: cols }, (_, i) => "-".padStart(maxCellSpace, "-")).join("-")}--`);
+	
+	logX_Axis();
+	logHorizontalLine();
 	grid.points.forEach((row, y) => {
 		console.log(`${y.toString().padStart(rowDigits, "0")} | ${row.map( cell => " ".repeat(maxCellSpace - 2) + (cell.visited ? "*" : " ") + cell.value).join(" ")}`);
 	});
+	logHorizontalLine();
+	logX_Axis();
+
 	console.log("")
 };
+
+// https://medium.com/@urna.hybesis/pathfinding-algorithms-the-four-pillars-1ebad85d4c6b
+export const aStar = <TValue = string>(
+	grid: Grid<TValue>,
+	start: PathNode,
+	end: PathNode,
+	movementDeltas: Array<MovementDelta>,	
+	movementCost: (current: PathNode, neighbor: PathNode) => number = (_current, _neighbor) => 1,
+	canMove: (neighbor: PathNode) => boolean = (neighbor) => grid.points[neighbor.y][neighbor.x].value != "#",
+	includeAllPaths: boolean = false
+): Array<Path> | undefined => {
+	// Step 1: Find openList node with lowest totalCost, move node to closedList
+	// Step 2: If currentNode = end, return path (reversed)
+	// Step 3: Get neighbors of currentNode
+	//		- If in closedList, skip
+	//		- If not in openList, calculate costs, set parent, add to openList
+	//		- If in openList and costFromStart is lower, update costFromStart and parent
+	// Step 4: Repeat
+	
+	start.costToFinish = manhattanDistance(start, end);
+
+	let isStartMovement = true;
+	const pathsFound: Array<PathNode> = [];
+	const openList = [start];
+	const closedList: Array<PathNode> = [];
+	const alternateParents: Record<string, Array<PathNode>> = {};
+	
+	while (openList.length > 0) {
+		const currentNode = openList.reduce((minNode, node) => node.totalCost < minNode.totalCost ? node : minNode, openList[0]);
+		const currentIndex = openList.indexOf(currentNode);
+		
+		openList.splice(currentIndex, 1);
+		closedList.push(currentNode);
+
+		if (currentNode.x === end.x && currentNode.y === end.y) {
+			// Only ever > 0 if includeAllPaths is true
+			if (pathsFound.length > 0) {
+				const currentCost = currentNode.totalCost;
+				
+				// If hit end with higher score than current, we can quit since we'll always find lowest to highest and will never find another lower than this
+				if (currentNode.totalCost > pathsFound[0].totalCost) break;
+			}
+
+			pathsFound.push(currentNode);
+			if (!includeAllPaths) break; // only want one path...
+
+			continue;
+		}
+
+		const neighbors = getNeighbors(grid, currentNode, movementDeltas.filter(d => isStartMovement || d.opposite != currentNode.direction));
+		isStartMovement = false;
+
+		for (const neighbor of neighbors) {
+			if (!canMove(neighbor)) continue;
+			const closedNeighbor = closedList.find(n =>
+				n.x == neighbor.x && n.y == neighbor.y &&
+				(!includeAllPaths || n.direction == neighbor.direction || n.direction == Movement.getOppositeDirection(neighbor.direction))
+			);
+			const costFromStart = currentNode.costFromStart + movementCost(currentNode, neighbor);
+
+			if (closedNeighbor) {
+				if (includeAllPaths && costFromStart == closedNeighbor.costFromStart) {
+					addAlternateParent(closedNeighbor.clone(currentNode), alternateParents);
+				}
+				continue;
+			}
+
+			const openNeighbor = openList.find(n => n.x == neighbor.x && n.y == neighbor.y);
+
+			if (!openNeighbor || (includeAllPaths && costFromStart <= openNeighbor.costFromStart)) {
+				neighbor.costFromStart = costFromStart;
+				neighbor.costToFinish = manhattanDistance(neighbor, end);
+				neighbor.parent = currentNode;
+				openList.push(neighbor);
+			}
+			else if (costFromStart < openNeighbor.costFromStart) {
+				// need to clone if all paths...
+				openNeighbor.costFromStart = costFromStart;
+				openNeighbor.parent = currentNode;
+			}
+		}
+	}
+
+	if (pathsFound.length == 0) return undefined;
+	
+	const allPaths: Array<Path> = [];
+
+	for (const endPath of pathsFound) {
+		allPaths.push(...getPaths(endPath, alternateParents));
+	}
+
+	return allPaths;
+};
+
+const manhattanDistance = <TValue = string>(a: Point<TValue> | PathNode, b: Point<TValue> | PathNode): number => Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
+
+export class Path {
+	nodes: Array<PathNode>;
+	get length(): number { return this.nodes.length; };
+	get totalCost(): number { return this.nodes[this.nodes.length - 1].totalCost; };
+	get lastNode(): PathNode { return this.nodes[this.nodes.length - 1]; };
+
+	constructor(nodes: Array<PathNode>) {
+		this.nodes = nodes;
+	}
+}
+export class PathNode {
+	x: number;
+	y: number;
+	costFromStart: number;
+	costToFinish: number;
+	get totalCost(): number { return this.costFromStart + this.costToFinish; };
+	direction: Direction;
+	parent?: PathNode;
+	pathIndex: number;
+
+	constructor(x: number, y: number, direction: Direction) {
+		this.x = x;
+		this.y = y;
+		this.direction = direction;
+		this.costFromStart = this.costToFinish = 0;
+		this.pathIndex = -1;
+	}
+
+	clone(parent: PathNode): PathNode {
+		const clone = new PathNode(this.x, this.y, this.direction);
+		clone.costFromStart = this.costFromStart;
+		clone.costToFinish = this.costToFinish;
+		clone.parent = parent;
+		return clone;
+	}
+}
+
+const getNeighbors = <TValue = string>(grid: Grid<TValue>, node: PathNode, movementDeltas: Array<MovementDelta>): Array<PathNode> => {
+	const neighbors = movementDeltas.map((delta) => {
+		const x = node.x + delta.dx;
+		const y = node.y + delta.dy;
+	
+		if (!grid.isInside([x, y])) return undefined;
+
+		return new PathNode(x, y, delta.direction);
+	}).filter(n => n != undefined);
+
+	return neighbors as Array<PathNode>;
+};
+
+const serializeNode = (node?: PathNode) => `${node?.x},${node?.y}`;
+
+const addAlternateParent = (node: PathNode, alternateParents: Record<string, Array<PathNode>>) => {
+	const key = serializeNode(node);
+	if (!alternateParents[key]) {
+		alternateParents[key] = [node];
+		return;
+	}
+	if (alternateParents[key].some(n => n.x == node.x && n.y == node.y)) return;
+	alternateParents[key].push(node);
+};
+
+const getPaths = (node: PathNode, alternateParents: Record<string, Array<PathNode>>): Array<Path> => {
+	const allPaths: Array<Path> = [];
+	const path: Array<PathNode> = [];
+	let current: PathNode | undefined = node;
+	while (current) {
+		path.push(current);
+		current = current.parent;
+
+		const alternateParent = alternateParents[serializeNode(current)];
+		
+		if (alternateParent) {
+			const pathToEnd = [...path].reverse();
+			for (const p of alternateParent) {
+				const alternatePaths = getPaths(p, alternateParents);
+				alternatePaths.forEach(p => allPaths.push(new Path([...p.nodes, ...pathToEnd])));
+			}
+		}
+	}
+	allPaths.push(new Path(path.reverse()));
+	return allPaths;
+};
+
+export type Direction = 'N' | 'E' | 'S' | 'W' | 'NE' | 'SE' | 'SW' | 'NW';
